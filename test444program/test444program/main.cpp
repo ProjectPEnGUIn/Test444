2583cd2d8be04637bc5bfb233c4bbd6f102f6bbf
ProjectPEnGUIn <t980409erma@edu.tibro.se>
2016-11-22 14:41:59 +0100

    added a program

---
+++ test444program/test444program/main.cpp
@@ -0,0 +1,14 @@
+//plis work
+
+#include <iostream>
+
+#include "variables.h"
+
+int main()
+{
+	if (a == 10)
+		std::cout << "a is 10\n";
+
+	return 0;
+
+}
---
+++ test444program/test444program/variables.h
@@ -0,0 +1,7 @@
+#pragma once
+#ifndef  _VARIABLES_
+#define _VARIABLES_
+
+int a = 10;
+
+#endif // ! _VARIABLES_
